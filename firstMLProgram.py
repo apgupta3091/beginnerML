cceda153782f610d2b3921aa0a3f56848ce8b0dc
Anuj Gupta <apgupta3091@gmail.com>
2020-11-19 01:09:40 -0500

    Added comments

--- firstMLProgram.py
+++ firstMLProgram.py
@@ -11,9 +11,12 @@ labels = [0, 0, 1, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
 
+#guessFruit = prediction value 1/0, 1 is an orange, 0 is an apple
 guessFruit = (clf.predict([[120, 0]]))
 
+#if guessFruit == [1] then print fruit is an Orange
 if guessFruit == [1]:
     print ("The Program predicts that the fruit is an Orange!")
+# else print fruit is an Apple
 else:
     print ("The Program predicts that the fruit is an Apple!")
