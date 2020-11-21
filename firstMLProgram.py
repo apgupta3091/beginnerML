356188269b0ddc4574e90d668a5043819dc5623e
Anuj Gupta <53449807+apgupta3091@users.noreply.github.com>
2020-11-21 13:38:40 -0500

    Update

--- firstMLProgram.py
+++ firstMLProgram.py
@@ -1,16 +1,13 @@
 from sklearn import tree
-
-#features = [[140, "smooth"], [130, "smooth"], [150, "bumpy"], [170, "bumpy"]]
-#labels = [ "apple", "apple", "orange", "orange" ]
-
-#[weight, 1 = smooth, 0 = bumpy]
+"""
+[weight, 1 = smooth, 0 = bumpy]
+0 = apple, 1 = orange
+"""
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
-# 0 = apple, 1 = orange
 labels = [0, 0, 1, 1]
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
-
 #guessFruit = prediction value 1/0, 1 is an orange, 0 is an apple
 guessFruit = (clf.predict([[120, 0]]))
 
