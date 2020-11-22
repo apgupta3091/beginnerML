9eb6546fb9ea5ef95f7566bfa51f65ab7294e243
Anuj Gupta <apgupta3091@gmail.com>
2020-11-21 23:56:15 -0500

    pineapple

--- firstMLProgram.py
+++ firstMLProgram.py
@@ -1,5 +1,4 @@
 from sklearn import tree
-<<<<<<< HEAD
 
 #features = [[140, "smooth"], [130, "smooth"], [150, "bumpy"], [170, "bumpy"]]
 #labels = [ "apple", "apple", "orange", "orange" ]
@@ -8,19 +7,11 @@ from sklearn import tree
 features = [[140, 1], [130, 1], [150, 0], [170, 0], [250, 2], [275, 2]]
 # 0 = apple, 1 = orange, 2 = pineapple
 labels = [0, 0, 1, 1, 2, 2]
-=======
-"""
-[weight, 1 = smooth, 0 = bumpy]
-0 = apple, 1 = orange
-"""
-features = [[140, 1], [130, 1], [150, 0], [170, 0]]
-labels = [0, 0, 1, 1]
->>>>>>> 356188269b0ddc4574e90d668a5043819dc5623e
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
 #guessFruit = prediction value 1/0, 1 is an orange, 0 is an apple
-guessFruit = (clf.predict([[260, 0]]))
+guessFruit = (clf.predict([[260, 2]]))
 
 #if guessFruit == [1] then print fruit is an Orange
 if guessFruit == [1]:
